9671e72ee3b0e4312d7c913f1e7a0ab6eca95661
katedoe <148048619+katedoe@users.noreply.github.com>
2023-10-26 09:34:28 +0200

    poprawka wg zalecen

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@ let num1 = 2;
 let num2 = 5;
 
 function more(arg1, arg2) {
-    if (arg1 == arg2) {console.log("liczby są równe")}
-    else if (arg1>arg2) {console.log(arg1)}
-    else if (arg1<arg2) {console.log(arg2)}
+    if (arg1 == arg2) {
+        return("liczby są równe")}
+    else if (arg1>arg2) {
+        return(arg1)}
+    else if (arg1<arg2) {
+        return(arg2)}
 }
 
 console.log(more (num1, num2));
